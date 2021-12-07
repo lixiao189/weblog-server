04643541e64133a5271f8f3a447e2f71d16ed180
Node Sans <node189@icloud.com>
2021-12-07 23:08:11 +0800

    修改了查看帖子的逻辑

--- app/controller/Post.php
+++ app/controller/Post.php
@@ -30,16 +30,10 @@ class Post
 
     public function getPost(Request $request, string $id): Response
     {
-        $session = $request->session();
-        $username = $session->get('username');
-        $isAdministrator = $session->get('administrator');
-
         $result = Db::table('posts')->where('id', '=', $id)->first();
 
         if (!isset($result)) {
             return responseData(2, '帖子不存在', null);
-        } else if ($isAdministrator == false && $result->sender_name != $username) { // 如果非管理员用户看不是自己的帖子
-            return responseData(1, '只能查看自己的帖子', null);
         } else {
             return responseData(0, '获取成功', [
                 'sender_name' => $result->sender_name,
